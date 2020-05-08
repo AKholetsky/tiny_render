4de2a3ea8307da395c4f15edc8af116eff09c236
Anton Kholetsky <kvazipolyr@gmail.com>
2020-05-08 15:47:17 +0300

    Triangles

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,75 @@
 use tgaimage::{TGAColor, TGAImage};
-use obj::{Obj, SimplePolygon, IndexTuple};
-use std::path::Path;
-
+use cgmath::Vector2;
 
 fn main() {
-    let african_head = Obj::<SimplePolygon>::load(&Path::new("resources/african_head.obj")).unwrap();
-
     let white = TGAColor::rgb(255, 255, 255);
+    let green = TGAColor::rgb(0, 255, 0);
+    let red = TGAColor::rgb(255, 0, 0);
+
 
     let width = 800;
     let height = 800;
     let mut image = TGAImage::new(width, height, 3);
 
-    for face in african_head.objects[0].groups[0].polys.iter() {
-        for i in 0..3 {
-            let IndexTuple(a, _, _) = face[i];
-            let IndexTuple(d, _, _) = face[(i + 1) % 3];
-            let v0 = african_head.position[a];    
-            let v1 = african_head.position[d];
+    // let mut t0: [Vector2<i32>; 3] = [Vector2::new(10, 70), Vector2::new(50, 160), Vector2::new(70, 80)]; 
+    // let mut t1: [Vector2<i32>; 3] = [Vector2::new(180, 50), Vector2::new(150, 1), Vector2::new(70, 180)]; 
+    // let mut t2: [Vector2<i32>; 3] = [Vector2::new(180, 150), Vector2::new(120, 160), Vector2::new(130, 180)]; 
 
-            let x0 = (v0[0] + 1.) * (width as f32 / 2.);
-            let y0 = (v0[1] + 1.) * (height as f32 / 2.);
-            let x1 = (v1[0] + 1.) * (width as f32 / 2.);
-            let y1 = (v1[1] + 1.) * (height as f32 / 2.);
-            line(x0 as i32, y0 as i32, x1 as i32, y1 as i32, &mut image, white);
-        }
-    }
+    triangle(Vector2::new(10, 70), Vector2::new(50, 160), Vector2::new(70, 80), &mut image, red);
+    triangle(Vector2::new(180, 50), Vector2::new(150, 1), Vector2::new(70, 180), &mut image, white);
+    triangle(Vector2::new(180, 150), Vector2::new(120, 160), Vector2::new(130, 180), &mut image, green);
+
     image.flip_vertically();
     image.write_tga_file("outpu.tga", true);
 }
 
-fn line(mut x0: i32, mut y0: i32, mut x1: i32, mut y1: i32, image: &mut TGAImage, color: TGAColor) {
+fn triangle(t0: Vector2<i32>, t1: Vector2<i32>, t2: Vector2<i32>, image: &mut TGAImage, color: TGAColor) {
+    let mut t0 = t0;
+    let mut t1 = t1;
+    let mut t2 = t2;
+    let green = TGAColor::rgb(0, 255, 0);
+    let red = TGAColor::rgb(255, 0, 0);
+    if t0.y > t1.y { std::mem::swap(&mut t0, &mut t1) };
+    if t0.y > t2.y { std::mem::swap(&mut t0, &mut t2) };
+    if t1.y > t2.y { std::mem::swap(&mut t1, &mut t2) };
+
+    let total_height = t2.y - t0.y;
+    for y in t0.y..t1.y {
+        let segment_height = t1.y - t0.y + 1;
+        let alpha = (y as f32 - t0.y as f32) / total_height as f32;
+        let beta = (y as f32 - t0.y as f32) / segment_height as f32;
+        let A = t0 + (t2 - t0) * alpha as i32;
+        let B = t0 + (t1 - t0) * beta as i32;
+        image.set(A.x as usize, y as usize, &red);
+        image.set(B.x as usize, y as usize, &green); 
+    }
+    // line(t0, t1, image, green);
+    // line(t1, t2, image, green);
+    // line(t2, t0, image, red);
+}
+
+fn line(p0: Vector2<i32>, p1: Vector2<i32>, image: &mut TGAImage, color: TGAColor) {
+    let mut p0 = p0;
+    let mut p1 = p1;
+
     let mut steep = false;
-    if (x0 - x1).abs() < (y0 - y1).abs() {
-        std::mem::swap(&mut x0, &mut y0);
-        std::mem::swap(&mut x1, &mut y1);
+    if (p0.x - p1.x).abs() < (p0.y - p1.y).abs() {
+        std::mem::swap(&mut p0.x, &mut p0.y);
+        std::mem::swap(&mut p1.x, &mut p1.y);
         steep = true;
     }
-
-    if x0 > x1 {
-        std::mem::swap(&mut x0, &mut x1);
-        std::mem::swap(&mut y0, &mut y1);
-    }
-
-    let dx = x1 - x0;
-    let dy = y1 - y0;
     
-    let derror2 = dy.abs() * 2;
-    let mut error2 = 0;
-    let mut y = y0;
-
-    for x in x0..=x1 {
+    if p0.x > p1.x {
+        std::mem::swap(&mut p0, &mut p1);
+    }
+    
+    for x in p0.x..=p1.x {
+        let t = (x as f32 - p0.x as f32)/(p1.x as f32 - p0.x as f32);
+        let y = p0.y as f32 * (1. - t) + p1.y as f32 * t;
         if steep {
             image.set(y as usize, x as usize, &color);
         } else {
             image.set(x as usize, y as usize, &color);    
-        }
-        error2 += derror2;
-        if error2 > dx {
-            y += if y1 > y0 { 1 } else  { -1 };
-            error2 -= dx * 2;
         }
     }
 }
